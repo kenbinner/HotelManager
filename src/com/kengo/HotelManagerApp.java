5cb52b38288e896b2129eb668acfd7cd685b408d
kenbinner <kengo@binner.org>
2023-04-01 12:14:19 +0100

    first commit

---
+++ src/com/kengo/HotelManagerApp.java
@@ -0,0 +1,18 @@
+package com.kengo;
+
+public class HotelManagerApp {
+
+	public static void main(String[] args) {
+		init();
+		
+		
+	}
+	
+	public static void init() {
+		System.out.println("===================");
+		System.out.println("=Hotel Manager App=");
+		System.out.println("===================");
+		System.out.println();
+	}
+
+}
---
+++ src/com/kengo/HotelManagerController.java
@@ -0,0 +1,5 @@
+package com.kengo;
+
+public class HotelManagerController {
+
+}
